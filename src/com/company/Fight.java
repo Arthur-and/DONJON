b34218b4a6a1de0d1349802b2392b5a63e10aca5
Arthur <arthur.andrieux@etu.univ-amu.fr>
2020-10-29 11:46:47 +0100

    method fight add

--- src/com/company/Fight.java
+++ src/com/company/Fight.java
@@ -4,12 +4,22 @@ import static java.lang.StrictMath.ceil;
 
 public class Fight {
 
-    public static int fight(Character character1, Character character2) {
+    public static boolean fight(Character character1, Character character2) {
         double number = ceil(Math.random()* 2 - 1);   // nb entre 0 et 1
 
+        while (true) {
+            character2.newVitality(character1.getDamage());
+            if (character2.getVitality() > 0)
+                character1.newVitality(character2.getDamage());
+            else
+                return true;
 
-        return 0;
+            character1.newVitality(character2.getDamage());
+            if (character1.getVitality() > 0)
+                character2.newVitality(character1.getDamage());
+            else
+                return false;
+        }
+
     }
-
-
 }
